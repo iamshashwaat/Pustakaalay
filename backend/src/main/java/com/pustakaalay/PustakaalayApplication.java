d43fbfc381095a95200b25e982f23280cf23d884
Shashwat Srivastav <shashwat1004@gmail.com>
2026-08-07 17:21:10 +0000

    Initialize Pustakaalay backend

---
+++ backend/src/main/java/com/pustakaalay/PustakaalayApplication.java
@@ -0,0 +1,13 @@
+package com.pustakaalay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PustakaalayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PustakaalayApplication.class, args);
+	}
+
+}
